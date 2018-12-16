a5e0bda4c9366938a3e7bb3ad93628f2eb4efb1c
Timo Schilling <timo@schilling.io>
2018-12-16 21:59:52 +0100

    don't require native ext

--- lib/jaro_winkler.rb
+++ lib/jaro_winkler.rb
@@ -2,8 +2,4 @@
 
 require 'jaro_winkler/version'
 
-if RUBY_ENGINE == 'ruby'
-  require 'jaro_winkler/jaro_winkler_ext'
-else
-  require 'jaro_winkler/jaro_winkler_pure'
-end
+require 'jaro_winkler/jaro_winkler_pure'
